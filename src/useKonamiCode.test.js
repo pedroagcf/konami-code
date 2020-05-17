fe69cc9c7e5abcaa76f7bba99fdd921d9d15001b
Pedro Augusto <pedro.augusto@osf.digital>
2020-05-16 22:25:33 -0300

    finalizado
    finalizado

---
+++ src/useKonamiCode.test.js
@@ -0,0 +1,67 @@
+import { renderHook, act } from '@testing-library/react-hooks'
+
+import useKonamiCode, { konamiSequence } from './useKonamiCode'
+
+describe("useKonamiCode", () => {
+	it("should return the user sequence", () => {
+		const { result } = renderHook(() => useKonamiCode());
+		act(() => {
+			window.dispatchEvent(new KeyboardEvent("keydown", { key: "ArrowUp" }));
+		})
+		expect(result.current.sequence).toEqual(["ArrowUp"])
+	});
+
+	it("should reset the sequence if the user types a wrong sequence", () => {
+		const { result } = renderHook(() => useKonamiCode());
+		act(() => {
+			window.dispatchEvent(new KeyboardEvent("keydown", { key: "ArrownUp" }))
+			window.dispatchEvent(new KeyboardEvent("keydown", { key: "k" }))
+		});
+		expect(result.current.sequence).toEqual([])
+	});
+
+	it("should return true when user types a right sequence", () => {
+		const { result } = renderHook(() => useKonamiCode());
+		konamiSequence.forEach((key) => {
+			act(() => {
+				window.dispatchEvent(new KeyboardEvent("keydown", { key }))
+			});
+		});
+		expect(result.current.rightSequence).toBe(true);
+	});
+
+	it("should return false when user types a wrong sequence", () => {
+		const { result } = renderHook(() => useKonamiCode());
+		act(() => {
+			window.dispatchEvent(new KeyboardEvent("keydown", { key: "k" }))
+		});
+		expect(result.current.rightSequence).toBe(false);
+	})
+
+	it("should allow diferente sequence and return if user types the sequence", () => {
+		const newSequence = ["j", "k"]
+		const { result } = renderHook(() => useKonamiCode(newSequence));
+
+		newSequence.forEach((key) => {
+			act(() => {
+				window.dispatchEvent(new KeyboardEvent("keydown", { key }))
+			});
+		});
+
+		expect(result.current.rightSequence).toBe(true);
+	})
+
+	it("should run a callback fn if right sequence if typed", () => {
+		const newSequence = ["j", "k"]
+		const callback = jest.fn();
+		const { result } = renderHook(() => useKonamiCode(newSequence, callback));
+
+		newSequence.forEach((key) => {
+			act(() => {
+				window.dispatchEvent(new KeyboardEvent("keydown", { key }))
+			});
+		});
+
+		expect(callback).toHaveBeenCalled();
+	})
+});
